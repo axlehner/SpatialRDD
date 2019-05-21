f3a5419eb96481532dafa0645fb22e6cf3286bf6
axlehner <ax.lehner@gmail.com>
2019-05-21 18:48:41 +0200

    dataset test

--- R/data.R
+++ R/data.R
@@ -13,3 +13,37 @@
 #' @docType data
 #' @source Lehner2019
 "cut_off.sf"
+
+
+#' Dataset with boundaries and polygons for the SpatialRDD vignette.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @docType data
+#' @format A data frame with 3 sf objects:
+#' \describe{
+#'   \item{test}{text}
+#'   \item{test}{text}
+#'   ...
+#' }
+#' @docType data
+#' @source Lehner2019
+"polygon_full.sf"
+
+
+#' Dataset with boundaries and polygons for the SpatialRDD vignette.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @docType data
+#' @format A data frame with 3 sf objects:
+#' \describe{
+#'   \item{test}{text}
+#'   \item{test}{text}
+#'   ...
+#' }
+#' @docType data
+#' @source Lehner2019
+"polygon_treated.sf"
